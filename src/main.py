87667bc201f234a58a9021fb383f24db25daaa92
Pavel <pavel.vorobyov@uzh.ch>
2024-09-23 18:34:46 +0200

    TESTTEST

--- src/main.py
+++ src/main.py
@@ -6,18 +6,39 @@ symbols = ["AAPL", "LDO.MI", "NVDA", "RHM.DE", "RTX", "AMZN", "BA", "UBSG.SW", "
               "GD", "AIR", "XOM", "SHEL", "CVX", "BP", "EC", "EQNR", "E", "PSX",
                 "WMT", "COST", "TGT", "HD", "CR", "CSV", "CA", "EBAY", "UHRN.SW", "MSFT", "GOOGL",
                   "IBM", "INTC", "CSCO", "ORCL", "ADBE", "CRM", "SSU.JO", "LOGN.SW", "ABBN.SW", "BKNG",
-                    "LVS", "DAL", "ABNB.SW", "AAL", "LHA.DE", "RYAAY", "GS", "UPS", "GE", "BLK", "VTI",
+                    "LVS", "DAL", "ABNB", "AAL", "LHA.DE", "RYAAY", "GS", "UPS", "GE", "BLK", "VTI",
                       "MS", "HLT", "SHOP", "SBUX", "BAER.SW", "SIE.DU", "ROG.SW", "TII.F"]
 
 
 
-stocks = {sym: yf.Ticker(sym) for sym in symbols}
+#stocks = {sym: yf.Ticker(sym) for sym in symbols}
 
-stocks["AAPL"]
+#stocks["AAPL"]
 
-data = stocks["AAPL"].history(period="1mo")
+#data = stocks["AAPL"].history(period="1mo")
 
 
 
-print(data)
-print(type(data))
+#print(data)
+#print(type(data))
+
+data = []
+
+
+for symbol in symbols:
+  ticker = yf.Ticker(symbol)
+  hist = ticker.history(period="1y", interval="1wk")
+
+  for date, row in hist.iterrows():
+    data.append({
+      'Date': date,
+      'Open': row['Open'],
+      'Symbol': symbol
+    })
+
+
+df = pd.DataFrame(data)
+
+
+df.head()
+
